8c026f9bd7f27d1362a44cd1f25a2f512913c334
bcrhbrhcdb <apet2804@mpsedu.org>
2024-10-04 07:44:21 -0500

    e

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-console.log('Hello!');
+require('dotenv').config()
+import { createClient } from '@supabase/supabase-js'
+
+const supabaseUrl = 'https://jzhsqrtnbnfpfuslzadd.supabase.co'
+const supabaseKey = process.env.SUPABASE_KEY
+const supabase = createClient(supabaseUrl, supabaseKey)
